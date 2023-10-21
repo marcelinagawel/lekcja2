3bd9d816b274fb1a6cc728335a81223e8b183a43
marcelinagawel <marcelina.gawel@interia.pl>
2023-10-21 11:42:49 +0200

    test commita

--- lekcja2/Program.cs
+++ lekcja2/Program.cs
@@ -2,9 +2,32 @@
 {
     internal class Program
     {
+        //statnic, co funkcja ma zwracac, nazwa, paramtry
+        //static void - nic nie zwraca
+        static int dodaj(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+
+
+        //true, false patrzysta niepatzysta
+        static bool isEven(int number)
+        {
+            if (number % 2 == 0)
+                return true;
+            return false;
+        }
+
+
+
+        //wywolywanie
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Wynik: " + dodaj(1, 2));
+            Console.WriteLine(isEven(9));
+
         }
     }
 }
